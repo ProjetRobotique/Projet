45823664b6d2e8c681ed21d6a618e31411eb919f
Kai-Luan <78020804+Kai-Luan@users.noreply.github.com>
2021-02-10 17:07:33 +0100

    Add files via upload

---
+++ simulation.py
@@ -0,0 +1,13 @@
+from robot import Robot
+from arene import Arene
+from fenetre import Fenetre
+
+# programme
+a=Arene()
+f= Fenetre(a)
+f.afficher()
+# binding de la fonction modifierTableau sur le canevas
+f.can.bind("<Button-1>", f.modifierTableau)
+
+# boucle principale
+f.init_window.mainloop()
