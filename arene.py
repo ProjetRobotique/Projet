8583032c102a4372b1ea6e4e1225f3458c68aaab
Kai-Luan <78020804+Kai-Luan@users.noreply.github.com>
2021-03-22 21:49:08 +0100

    deplacement par la vitesse roues dans updateArene

--- arene.py
+++ arene.py
@@ -1,17 +1,14 @@
 # coding: utf-8
 from robot import *
-from math import pi as PI
+import math
 import time
 
 TAILLE_ARENE_X=25
 TAILLE_ARENE_Y=25
 
 class Arene:
-	def __init__(self, control, robot):
+	def __init__(self, robot):
 		self.exit= False
-
-		# Controle du Robot
-		self.control= control
 
 		# matrice à deux dimensions
 		self.tableau = []
@@ -21,6 +18,7 @@ class Arene:
 		# Ajout du Robot dans l'Arène
 		self.robot= robot
 		self.robot.pos = [10.0,10.0]
+		self.angle=0
 		pos= self.robot.pos
 		self.tableau[int(pos[0])][int(pos[1])]=2 #conversion des floats en entier
 	
@@ -41,9 +39,42 @@ class Arene:
 		while True:
 			if self.exit:
 				break
-			self.updateArene()
+			self.updateArene(fps)
 			time.sleep(1./fps)
 
-	def updateArene(self):
-		if self.control.enMarche:
-			self.avancerRobot()
+	def updateArene(self,fps):
+		tab = self.robot.vitesse_roue
+		rayonRoue= self.robot.rayonRoue
+		rayonRobot= self.robot.rayonRobot
+		x=self.robot.pos[0]
+		y=self.robot.pos[1]
+
+		vitesse_avance= min(tab[0], tab[1])
+		vitesse_tourne= tab[1]-tab[0]
+
+		# Calcule de la position du Robot 
+		distance= (math.pi*vitesse_avance*rayonRoue)/(180.0*fps)
+		x+= distance*math.cos(self.angle*math.pi/180.0)
+		y+= distance*math.sin(self.angle*math.pi/180.0)
+
+		# Calcule de l'angle du Robot
+		self.angle= (self.angle+((rayonRoue*vitesse_tourne*1.0)/(fps*rayonRobot)))%360
+		self.robot.angle= self.angle
+
+		# On vérifie si le robot sort du tableau en abscisse
+		if x>=TAILLE_ARENE_X:
+			x= TAILLE_ARENE_X-1
+		elif self.robot.pos[0]<0:
+			x=0
+		# On vérifie si le robot sort du tableau en ordonnée
+		if y>=TAILLE_ARENE_Y:
+			y= TAILLE_ARENE_Y-1
+		elif y<0:
+			y=0
+		# On vérifie s'il y a un obstacle
+		if self.tableau[int(x)][int(y)]!=1:
+			self.tableau[int(self.robot.pos[0])][int(self.robot.pos[1])]=0
+			self.tableau[int(x)][int(y)]=2
+			self.robot.pos= [x,y]
+
+
