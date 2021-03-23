5610d4a1d6fe9d8ecd9783513144799280410279
Kai-Luan <78020804+Kai-Luan@users.noreply.github.com>
2021-03-23 12:27:02 +0100

    Correction des stratégies

--- strategy.py
+++ strategy.py
@@ -12,15 +12,14 @@ class StrategyAvance:
 		self.robot.changerVitesseRoue(1, "LEFT")
 		self.robot.changerVitesseRoue(1, "RIGHT")
 		self.distanceCourant+=(math.pi*vitesse_avance*rayonRoue)/(180.0*fps)
-		if self.stop():
-			self.robot.changerVitesseRoue(0, "LEFT")
-			self.robot.changerVitesseRoue(0, "RIGHT")
 
 	def start(self):
 		self.distanceCourant=0
 
 	def stop(self):
 		if self.distanceCourant>= self.distance:
+			self.robot.changerVitesseRoue(0, "LEFT")
+			self.robot.changerVitesseRoue(0, "RIGHT")
 			return True
 		return False
 
@@ -28,23 +27,23 @@ class StrategyAvance:
 class StrategyTourneGauche:
 	def __init__(self, robot):
 		self.robot= robot
-		self.angle=0
-		self.angleCourant=90
+		self.angleCourant=0
+		self.angle=90
 
 	def run(self, fps):
 		rayonRoue= self.robot.rayonRoue
 		rayonRobot= self.robot.rayonRobot
-		vitesse_tourne= 1
-		self.robot.changerVitesseRoue(1, "RIGHT")
+		vitesse_tourne= 10
+		self.robot.changerVitesseRoue(vitesse_tourne, "RIGHT")
 		# Calcule de l'angle du Robot
-		self.angleCourant= (self.angleCourant+((rayonRoue*vitesse_tourne*1.0)/(fps*rayonRobot)))%360
-		if self.stop():
-			self.robot.changerVitesseRoue(0, "RIGHT")
+		self.angleCourant+= (rayonRoue*vitesse_tourne*1.0)/(fps*rayonRobot)
 
 	def start(self):
 		self.angleCourant=0
 
 	def stop(self):
+		print(self.angleCourant)
 		if self.angleCourant>= self.angle:
+			self.robot.changerVitesseRoue(0, "RIGHT")
 			return True
 		return False
